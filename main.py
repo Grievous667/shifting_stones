839fa57ac2328c7e8a035a3c4057cceb61dfbc9b
Grievous667 <130871077+Grievous667@users.noreply.github.com>
2024-01-25 10:10:08 -0500

    Reorganized code + implemented correct tile generation
    - Moved input handling to main
    - Fixed exit code so the .exe doesn't throw errors
    - Tile swapping takes grid coordinates as argument
    - Face img/backup are consistent now
    - Swap/flip code is simplified 
    - Excess variables across the program removed/general simplification
    - Nomenclature changed to be more consistent with the game
    - Tile generation moved to grid object + generates the correct tiles

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import pygame
+import sys
 
-from src.slot import Slot
+from src.grid import GameGrid
+
 
 class PygameEnvironment():
     def __init__(self, sx, sy, caption, mode=0) -> None:
         self.caption = caption
-        self.sx = sx
-        self.sy = sy
+        self.sx = sx # Screen width
+        self.sy = sy # Screen height
 
         self.running = True
         self.can_click = True
 
         pygame.init()
         pygame.display.set_caption(self.caption)
-        self.s = pygame.display.set_mode((self.sx, self.sy))
-        self.bg = pygame.Surface((self.sx, self.sy))
+        self.s = pygame.display.set_mode((self.sx, self.sy)) # Pygame display
+        self.bg = pygame.Surface((self.sx, self.sy)) # Background surface
 
-        self.grid = []
-        gx = Slot.tile_size
-        gy = Slot.tile_size / 2
-        
-        for i in range(3): 
-            self.grid.append([])
-            for _ in range(3):
-                self.grid[i].append(Slot(self.s, gx, gy))
-                gx += Slot.tile_size + 10
-            gx = Slot.tile_size
-            gy += Slot.tile_size + 10
+        self.grid = GameGrid(self.s) # The grid object manages the tiles. It handles tile initialization, graphics, and has a grab function that returns a tile by inputted coordinate. 
+        self.selection = None # Currently selected tile.
 
         while self.running == True:
             self.mainloop()
@@ -37,36 +30,69 @@ class PygameEnvironment():
     def mainloop(self):
         self.catch_events()
         self.get_input()
-        self.restrict_clicks()
+        
         self.game_logic()
+        self.restrict_clicks()
 
         pygame.display.update()
 
-    def catch_events(self):
+    def catch_events(self): # Pygame template code. Close the window if the x is clicked.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.display.quit
                 pygame.quit()
                 self.exit()
-                exit()
+                sys.exit()
+                return
 
-    def get_input(self):
+    def get_input(self): # Pygame input handlers 
         self.keys = pygame.key.get_pressed()
         self.mouse_pos = pygame.mouse.get_pos()
 
-    def restrict_clicks(self):
-        if pygame.mouse.get_pressed()[0]:
+    def restrict_clicks(self): # self.can_click restricts every handled mouse input/keystroke to call only once when pressed. The input must be released before another can be called.
+        if pygame.mouse.get_pressed()[0] or self.keys[pygame.K_LEFT] or self.keys[pygame.K_RIGHT] or self.keys[pygame.K_UP] or self.keys[pygame.K_DOWN]or self.keys[pygame.K_SPACE]:
             self.can_click = False
-        else:
-            self.can_click = True
+        else: self.can_click = True 
 
     def game_logic(self):
+        # Draw the background and tiles.
         self.s.blit(self.bg, [0,0])
-        for r in self.grid:
-            for g_item in r:
-                g_item.draw()
-                self.grid = g_item.logic(self.mouse_pos, self.keys, self.grid)
-            
+        self.grid.draw_tiles()
 
+        # This handles tile selection. When the mouse is clicked, check every tile for a collision and select if that's the case. Unselect any tiles that aren't colliding.
+        if pygame.mouse.get_pressed()[0] and self.can_click == True:
+            self.selection = None
+            for tile in self.grid.active_tiles:
+                tile.selected = False
+                if tile.rect.collidepoint(self.mouse_pos):
+                    tile.selected = True
+                    self.selection = tile                    
+            self.can_click = False
+        
+        # This handles tile movement/flipping. Tiles are grabbed/drawn by position on the 3x3 grid. 
+        if self.selection != None:
+            if self.can_click == True:
+                if self.keys[pygame.K_LEFT]: 
+                    if self.selection.pos[0] != 0:
+                        self.selection.swap(self.grid.grab_tile([self.selection.pos[0]-1, self.selection.pos[1]]))
+                    self.can_click = False
+                elif self.keys[pygame.K_RIGHT]: 
+                    if self.selection.pos[0] != 2:
+                        self.selection.swap(self.grid.grab_tile([self.selection.pos[0]+1, self.selection.pos[1]]))
+                    self.can_click = False
+                elif self.keys[pygame.K_UP]: 
+                    if self.selection.pos[1] != 0:
+                        self.selection.swap(self.grid.grab_tile([self.selection.pos[0], self.selection.pos[1]-1]))
+                    self.can_click = False
+                elif self.keys[pygame.K_DOWN]: 
+                    if self.selection.pos[1] != 2:
+                        self.selection.swap(self.grid.grab_tile([self.selection.pos[0], self.selection.pos[1]+1]))
+                    self.can_click = False
+
+                elif self.keys[pygame.K_SPACE]: 
+                    self.selection.flip()
+                    self.can_click = False
+        
 if __name__ == '__main__':
     print(__file__)      
     PygameEnvironment(1000, 750, 'Shifting Stones', 0)
