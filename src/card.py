c8188e84005732bd732f033b930d0dd215b43597
Grievous667 <130871077+Grievous667@users.noreply.github.com>
2024-01-25 10:11:08 -0500

    Update and rename card.py to tile.py

--- src/card.py
+++
@@ -1,8 +0,0 @@
-class Card():
-    tile_size = 200
-    
-    def __init__(self) -> None:
-        self.flipped = False
-        self.selected = False
-        self.can_flip = True
-        self.can_switch = True
---
+++ src/tile.py
@@ -0,0 +1,24 @@
+class Tile():
+    tilelist = []
+    tile_size = 200
+    def __init__(self, subclass, pos=[0,0]) -> None:
+        self.type = subclass()
+        self.pos = pos
+        self.flipped = False
+
+        self.selected = False
+        self.rect = None
+
+    def swap(self, other_tile):
+        pos_copy = self.pos
+        self.pos = other_tile.pos
+        other_tile.pos = pos_copy
+
+    def flip(self):
+        if self.flipped == False:
+            self.flipped = True
+            return True
+        elif self.flipped == True:
+            self.flipped = False
+            return False
+        
